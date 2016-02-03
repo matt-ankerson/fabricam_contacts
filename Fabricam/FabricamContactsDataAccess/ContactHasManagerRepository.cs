b46d3b59da2719254fb27b9b9556effa7c87c0c0
Matt Ankerson <MattA@intergen.org.nz>
2016-02-03 22:52:03 +1300

    Repository pattern implemented for both models

---
+++ Fabricam/FabricamContactsDataAccess/ContactHasManagerRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricamContactsDataAccess
+{
+    /// <summary>
+    /// Repository pattern for facilitating access to the data.
+    /// </summary>
+    public class ContactHasManagerRepository
+    {
+        private FabricamContactsDbContext context;
+        private bool disposed = false;
+
+        public ContactHasManagerRepository(FabricamContactsDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<ContactHasManager> GetContactHasManagers()
+        {
+            return context.ContactHasManagers.ToList();
+        }
+
+        public ContactHasManager GetContactHasManagerById(int contactHasManagerId)
+        {
+            return context.ContactHasManagers.Find(contactHasManagerId);
+        }
+
+        public void InsertContactHasManager(ContactHasManager contactHasManager)
+        {
+            context.ContactHasManagers.Add(contactHasManager);
+        }
+
+        public void DeleteContactHasManager(int contactHasManagerId)
+        {
+            ContactHasManager contactHasManagerToDelete = context.ContactHasManagers.Find(contactHasManagerId);
+            context.ContactHasManagers.Remove(contactHasManagerToDelete);
+        }
+
+        public void UpdateContactHasManager(ContactHasManager contactHasManager)
+        {
+            context.Entry(contactHasManager).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Fabricam/FabricamContactsDataAccess/IContactHasManagerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricamContactsDataAccess
+{
+    public interface IContactHasManagerRepository : IDisposable
+    {
+        IEnumerable<ContactHasManager> GetContactHasManagers();
+        ContactHasManager GetContactHasManagerById(int contactHasManagerId);
+        void InsertContactHasManager(ContactHasManager contactHasManager);
+        void DeleteContactHasManager(int contactHasManagerId);
+        void UpdateContactHasManager(ContactHasManager contactHasManager);
+        void Save();
+    }
+}
