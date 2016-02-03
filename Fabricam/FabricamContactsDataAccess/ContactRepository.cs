8b1bfd7524e89513ebbd3fda5ef0b7be1cff566b
Matt Ankerson <MattA@intergen.org.nz>
2016-02-03 22:33:20 +1300

    implementing repository pattern for data access

---
+++ Fabricam/FabricamContactsDataAccess/ContactRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricamContactsDataAccess
+{
+    /// <summary>
+    /// Repository pattern for facilitating access to the data.
+    /// </summary>
+    public class ContactRepository : IContactRepository
+    {
+        private FabricamContactsDbContext context;
+        private bool disposed = false;
+
+        public ContactRepository(FabricamContactsDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Contact> GetContacts()
+        {
+            return context.Contacts.ToList();
+        }
+
+        public Contact GetContactById(int contactId)
+        {
+            return context.Contacts.Find(contactId);
+        }
+
+        public void InsertContact(Contact contact)
+        {
+            context.Contacts.Add(contact);
+        }
+
+        public void DeleteContact(int contactId)
+        {
+            Contact contactToDelete = context.Contacts.Find(contactId);
+            context.Contacts.Remove(contactToDelete);
+        }
+
+        public void UpdateContact(Contact contact)
+        {
+            context.Entry(contact).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Fabricam/FabricamContactsDataAccess/IContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricamContactsDataAccess
+{
+    public interface IContactRepository : IDisposable
+    {
+        IEnumerable<Contact> GetContacts();
+        Contact GetContactById(int contactId);
+        void InsertContact(Contact contact);
+        void DeleteContact(int contactId);
+        void UpdateContact(Contact contact);
+        void Save();
+    }
+}
