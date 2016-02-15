f6d952369b66c9adab804d8cb0ce2e09c5bcddc8
Matt Ankerson <MattA@intergen.org.nz>
2016-02-15 16:51:56 +1300

    removed csv export bug, pending better fix

--- Fabricam/Fabricam/Scripts/exporttocsv.js
+++ Fabricam/Fabricam/Scripts/exporttocsv.js
@@ -6,13 +6,13 @@
             var tmpArr = [];
             var tmpStr = '';
             data.find("tr").each(function () {
-                if ($(this).find("th").length) {
-                    $(this).find("th").each(function () {
-                        tmpStr = $(this).text().replace(/"/g, '""');
-                        tmpArr.push('"' + tmpStr + '"');
-                    });
-                    csvData.push(tmpArr);
-                } else {
+                //if ($(this).find("th").length) {
+                //    $(this).find("th").each(function () {
+                //        tmpStr = $(this).text().replace(/"/g, '""');
+                //        tmpArr.push('"' + tmpStr + '"');
+                //    });
+                //    csvData.push(tmpArr);
+                //} else {
                     tmpArr = [];
                     $(this).find("td").each(function () {
                         if ($(this).text().match(/^-{0,1}\d*\.{0,1}\d+$/)) {
@@ -23,7 +23,7 @@
                         }
                     });
                     csvData.push(tmpArr.join(','));
-                }
+                //}
             });
             var output = csvData.join('\n');
             var uri = 'data:application/csv;charset=UTF-8,' + encodeURIComponent(output);
